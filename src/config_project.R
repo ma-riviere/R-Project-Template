1c53a101bdebc31928b5a91a1be6ba39f0eb27d1
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2021-09-18 13:14:03 +0200

    Update config_project.R

--- src/config_project.R
+++ src/config_project.R
@@ -5,7 +5,7 @@
 cat(main("\n[CONFIG] Loading Project Configs ...\n"))
 
 config <- config::get()
-secret <- config::get("secret.yml")
+secret <- config::get(file = "secret.yml")
 
 options(
   contrasts = c("contr.sum", "contr.poly")
