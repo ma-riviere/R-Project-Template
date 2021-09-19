d441317e32baf328b476fafb00d46412d21e6608
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2021-09-19 20:20:42 +0200

    Improve project setup

--- src/setup_project.R
+++ src/setup_project.R
@@ -2,9 +2,43 @@
 #### Project Setup ####
 #=====================#
 
+cat("\n>> [SETUP] Starting Project Setup ... <<\n")
+
+if(!"renv" %in% utils::installed.packages()) {install.packages("renv"); library(renv)}
+
+if(is.null(renv::project())) {
+  renv::init(project = here::here(), bare = TRUE, restart = FALSE)
+  if(!file.exists(here::here("config.yml"))) {
+    file.create(here::here("config.yml"))
+    cat('default:\r  data: !expr here::here("data", "my_data.csv")\r', file = here::here("config.yml"))
+  }
+  if(!file.exists(here::here("secret.yml"))) {
+    file.create(here::here("secret.yml"))
+    cat('default:\r', file = here::here("secret.yml"))
+  }
+}
+
+# TODO: usethis::use_blank_slate(scope = "project") <- only once ? no prompt ?
+
+if(length(list.files(here::here("src", "common"))) == 0) system(here::here("update_commons.bat"), intern = TRUE)
+
+source(here::here("src", "common", "setup_global.R"), echo = F)
+
+source(here::here("src", "common", "packman.R"), echo = F)
+init_base_packages()
+
+source(here::here("src", "common", "utils.R"), echo = F)
+
+source(here::here("src", "common", "config_global.R"), echo = F)
+
+source(here::here("src", "common", "theme.R"), echo = F)
+
+#--------------------------------#
+#### Project-specific scripts ####
+#--------------------------------#
+
 setup_project <- function(update = FALSE) {
-  cat(main("\n>> [SETUP] Starting Project Setup ... <<\n"))
-  
+
   source(here::here("src", "packages.R"), echo = F)
   
   init_project_packages(update = update)
