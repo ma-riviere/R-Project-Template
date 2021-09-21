41d955ded12c8e8dffa861dfc64876f1e8346364
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2021-09-21 11:26:45 +0200

    Update submodule handling

--- src/setup_project.R
+++ src/setup_project.R
@@ -12,15 +12,21 @@ if(is.null(renv::project())) {
     file.create(here::here("config.yml"))
     cat('default:\r  data: !expr here::here("data", "my_data.csv")\r', file = here::here("config.yml"))
   }
-  if(!file.exists(here::here("secret.yml"))) {
-    file.create(here::here("secret.yml"))
-    cat('default:\r  api_key: ""\r', file = here::here("secret.yml"))
-  }
+}
+
+if(!file.exists(here::here("secret.yml"))) {
+  file.create(here::here("secret.yml"))
+  cat('default:\r  api_key: ""\r', file = here::here("secret.yml"))
+}
+
+update_submodules <- function() {
+  if(Sys.info()[["sysname"]] == "Linux") system(here::here("update_commons.sh"), intern = TRUE)
+  else if(Sys.info()[["sysname"]] == "Windows") system(here::here("update_commons.bat"), intern = TRUE)
 }
 
 # TODO: usethis::use_blank_slate(scope = "project") <- only once ? no prompt ?
 
-if(length(list.files(here::here("src", "common"))) == 0) system(here::here("update_commons.bat"), intern = TRUE)
+if(length(list.files(here::here("src", "common"))) == 0) update_submodules()
 
 source(here::here("src", "common", "packman.R"), echo = F)
 init_base_packages()
@@ -28,6 +34,7 @@ init_base_packages()
 source(here::here("src", "common", "utils.R"), echo = F)
 
 source(here::here("src", "common", "config_global.R"), echo = F)
+load_global_config()
 
 source(here::here("src", "common", "theme.R"), echo = F)
 
@@ -46,6 +53,8 @@ setup_project <- function(update = FALSE) {
   source(here::here("src", "data.R"), echo = F)
   
   source(here::here("src", "viz.R"), echo = F)
+
+  source(here::here("src", "stats.R"), echo = F)
   
   cat(main("\n>> [SETUP] Project Setup DONE ! <<\n"))
 }
