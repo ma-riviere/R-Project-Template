50e051d03f27c1c0e3a6e5121a8bb39cb37304e2
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2021-10-19 17:06:58 +0200

    Update config + commons

--- src/config_project.R
+++ src/config_project.R
@@ -7,10 +7,13 @@ cat(main("\n[CONFIG] Loading Project Configs ...\n"))
 config <- config::get()
 secret <- config::get(file = "secret.yml")
 
+my_palettes_c <- list("viridis")
+my_palettes_d <- list("viridis_d")
+
 options(
   contrasts = c("contr.sum", "contr.poly"),
-  ggplot2.discrete.colour = "viridis_d",
-  ggplot2.discrete.fill = "viridis_d",
-  ggplot2.continuous.colour = "viridis",
-  ggplot2.continuous.fill = "viridis"
+  ggplot2.discrete.colour = my_palettes_d,
+  ggplot2.discrete.fill = my_palettes_d,
+  ggplot2.continuous.colour = my_palettes_c,
+  ggplot2.continuous.fill = my_palettes_c
 )
