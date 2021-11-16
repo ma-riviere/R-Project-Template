87252faa46d2350b24e929f6e531713ea4e5484d
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2021-11-16 16:15:02 +0100

    Update default colors + repos

--- src/config_project.R
+++ src/config_project.R
@@ -7,13 +7,17 @@ cat(main("\n[CONFIG] Loading Project Configs ...\n"))
 config <- config::get()
 secret <- config::get(file = "secret.yml")
 
-my_palettes_c <- list("viridis")
-my_palettes_d <- list("viridis_d")
+## TODO: explore color palettes packages (lisa, ...)
+
+# my_colors <- c("#777777", "#ff6000")
+# my_palette_d <- list(my_colors, scales::viridis_pal()(2), scales::viridis_pal()(5), scales::viridis_pal()(10))
 
 options(
   contrasts = c("contr.sum", "contr.poly"),
-  ggplot2.discrete.colour = my_palettes_d,
-  ggplot2.discrete.fill = my_palettes_d,
-  ggplot2.continuous.colour = my_palettes_c,
-  ggplot2.continuous.fill = my_palettes_c
+  ggplot2.discrete.colour = \() scale_color_viridis_d(), # my_palette_d
+  ggplot2.discrete.fill = \() scale_fill_viridis_d(), # my_palette_d
+  ggplot2.continuous.colour = \() scale_color_viridis_c(),
+  ggplot2.continuous.fill = \() scale_fill_viridis_c(),
+  ggplot2.binned.colour = \() scale_color_viridis_b(),
+  ggplot2.binned.fill = \() scale_fill_viridis_b()
 )
--- src/packages.R
+++ src/packages.R
@@ -1,3 +1,9 @@
+project_repos <- c(
+  # EasyStats = "https://easystats.r-universe.dev",
+  # Stan = "https://mc-stan.org/r-packages/",
+  CRAN = "https://cloud.r-project.org/"
+)
+
 project_pkgs <- c(
   ### Pre-requisites
   
